173c457a6ea958170e0476c3ebebf1bc1d01ee67
juanequex <ramon.g.uz37@gmail.com>
2023-01-30 14:05:50 -0600

    initial project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import './stylesheets/application.scss'
+
+function App() {
+  return (
+    <div className='container'>
+      <h1 className='container__title'>Hello World!!</h1>
+    </div>
+  );
+}
+
+export default App;
